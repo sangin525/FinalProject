0036869b4df38d318d6d40f09bad8ad36763ecc4
leesangin <sangin525@naver.com>
2024-01-22 14:42:17 +0900

    회원가입 폼 유효성 검사 및 주소 API적용

--- FinalProject/src/main/webapp/WEB-INF/views/member/register.jsp
+++ FinalProject/src/main/webapp/WEB-INF/views/member/register.jsp
@@ -6,6 +6,8 @@
 <html>
 <head>
 	<%@ include file="../../views/common/head.jsp" %>
+	<link rel="stylesheet" href="/resources/css/member/member.css">
+	<script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
 </head>
 	<%@ include file="../../views/common/header.jsp" %>
 
@@ -13,37 +15,70 @@
 	<div class="registerContain">
 		<h2 class="register_h2">회원가입</h2>
 		<form class="registerForm">
+		<div class = "postCodeFind">
 			<div class="memberInputbox">
-				<input type="text" id="email" placeholder="이메일을 입력해주세요.">
+				<input type="email" id="email" onkeyup="validateEmail()" autocomplete="off">
 				<label for="email" class="register_label">이메일</label>
+				<span id="emailMsg"></span>
 			</div>
+				<button class="registerFormBtn" onclick="" type="button">중복확인</button>
+		</div>
 			<div class="memberInputbox">
-				<input type="text" id="password" placeholder="이메일을 입력해주세요.">
+				<input type="password" id="password" onkeyup="validatePassword()" autocomplete="off">
 				<label for="password" class="register_label">비밀번호</label>
+				<span id="passwordHelpInline" class="form-text"> 비밀번호는
+				8~20자 길이로 문자와 숫자와 특수문자[@$!%*?&\#]를 포함해야 하며 공백, 이모티콘을 포함할 수 없습니다. </span><br>
+				<span id="PwdMsg"></span>
 			</div>
 			<div class="memberInputbox">
-				<input type="text" id="email" placeholder="이메일을 입력해주세요.">
+				<input type="password" id="passwordCheck" onkeyup="validatePassword()" autocomplete="off">
+				<label for="passwordCheck" class="register_label">비밀번호확인</label>
+				<span id="PwdcheckMsg"></span>
+			
+			</div>
+			<div class="memberInputbox">
+				<input type="text" id="name" autocomplete="off">
 				<label for="name" class="register_label">이름</label>
 			</div>
+			<div class = "postCodeFind">
 			<div class="memberInputbox">
-				<input type="text" id="nickName" placeholder="이메일을 입력해주세요.">
+				<input type="text" id="nickName" autocomplete="off">
 				<label for="nickName" class="register_label">닉네임</label>
 			</div>
+				<button class="registerFormBtn" onclick="" type="button">중복확인</button>
+			</div>
 			<div class="memberInputbox">
-				<input type="text" id="phone" placeholder="이메일을 입력해주세요.">
+				<input type="text" id="phone" autocomplete="off">
 				<label for="phone" class="register_label">전화번호</label>
 			</div>
 			<div class="memberInputbox">
-				<input type="text" id="birth" placeholder="이메일을 입력해주세요.">
+				<input type="text" id="birth" autocomplete="off">
 				<label for="birth" class="register_label">생년월일</label>
 			</div>
 			<div class="memberInputbox">
-				<input type="text" id="gender" placeholder="이메일을 입력해주세요.">
+				<input type="text" id="gender" autocomplete="off">
 				<label for="gender" class="register_label">성별</label>
 			</div>
+			<div class = "postCodeFind">
+				<div class="memberInputbox">
+					<input type="text" id="postcode" autocomplete="off">
+					<label for="postcode" class="register_label">우편번호</label>
+				</div>
+				<button type="button" onclick="execDaumPostcode()" class="registerFormBtn">우편번호 찾기</button>
+			</div>
 			<div class="memberInputbox">
-				<input type="text" id="addr" placeholder="이메일을 입력해주세요.">
-				<label for="addr" class="register_label">주소</label>
+				<input type="text" id="address" autocomplete="off">
+				<label for="address" class="register_label">주소</label>
+			</div>
+			<div class="postCodeFind">
+			<div class="memberInputbox">
+				<input type="text" id="detailAddress" autocomplete="off">
+				<label for="detailAddress" class="register_label">상세주소</label>
+			</div>
+			<div class="memberInputbox" id="exAddr">
+				<input type="text" id="extraAddress" autocomplete="off">
+				<label for="extraAddress" class="register_label">참고항목</label>
+			</div>
 			</div>
 			<br>
 			<div class="btn_group">
@@ -54,12 +89,3 @@
 
 </html>
 
-
-
-
-
-
-
-
-
-</html>
---
+++ FinalProject/src/main/webapp/resources/js/member/register.js
@@ -0,0 +1,140 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var inputs = document.querySelectorAll('.memberInputbox input');
+    
+    // 페이지 로딩 시 모든 입력란 검사
+    inputs.forEach(function(input) {
+        handleLabelPosition(input);
+        
+        // 각 입력란에 이벤트 리스너 추가
+        input.addEventListener('input', function() { handleLabelPosition(input); });
+        input.addEventListener('focus', function() { handleLabelPosition(input); });
+        input.addEventListener('blur', function() { handleLabelPosition(input); });
+    });
+});
+
+function handleLabelPosition(input) {
+    var label = input.nextElementSibling;
+    // 입력란에 값이 있거나 포커스가 있는 경우
+    if (input.value.trim() !== '' || input === document.activeElement) {
+        label.style.top = '0px';
+        label.style.fontSize = '10pt';
+        label.style.color = '#8aa1a1';
+    } else {
+        // 입력란이 비어있고 포커스가 없는 경우
+        label.style.top = '15px';
+        label.style.fontSize = '14pt';
+        label.style.color = '#aaa';
+    }
+}
+
+function execDaumPostcode() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            // 주소 변수
+            var addr = '';
+            // 참고항목 변수
+            var extraAddr = '';
+
+            // 사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            if (data.userSelectedType === 'R') {
+                addr = data.roadAddress;
+            } else {
+                addr = data.jibunAddress;
+            }
+
+            // 사용자가 도로명 주소를 선택했을 경우 참고항목을 조합한다.
+            if(data.userSelectedType === 'R'){
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraAddr += data.bname;
+                }
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                if(extraAddr !== ''){
+                    extraAddr = ' (' + extraAddr + ')';
+                }
+            }
+
+            // 우편번호, 주소, 참고항목 필드를 가져온다.
+            var postcodeInput = document.getElementById('postcode');
+            var addressInput = document.getElementById("address");
+            var extraAddressInput = document.getElementById("extraAddress");
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            postcodeInput.value = data.zonecode;
+            addressInput.value = addr;
+            extraAddressInput.value = extraAddr;
+
+            // 각 입력란에 대해 'input' 이벤트를 강제로 발생시킨다.
+            triggerInputEvent(postcodeInput);
+            triggerInputEvent(addressInput);
+            triggerInputEvent(extraAddressInput);
+
+            // 커서를 상세주소 필드로 이동한다.
+            document.getElementById("detailAddress").focus();
+        }
+    }).open();
+}
+
+function triggerInputEvent(input) {
+    var event = new Event('input', {
+        bubbles: true,
+        cancelable: true,
+    });
+    input.dispatchEvent(event);
+}
+function moveLabelAbove(input) {
+    var label = input.nextElementSibling;
+    if (input.value.trim() !== '') {
+        label.classList.add('active');
+    } else {
+        label.classList.remove('active');
+    }
+}
+
+// 비밀번호 유효성 검사 + 비밀번호 확인하고 같은지
+    function validatePassword() {
+       const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[@$!%*?&\#])[A-Za-z\d@$!%*?&\#]{8,20}$/;
+       const password = document.getElementById("password").value;
+       const msg = document.getElementById("PwdMsg");
+       
+       if(password == "") {
+           msg.innerHTML = "비밀번호를 입력하세요.";
+           msg.style.color = "red";
+       } else if(passwordRegex.test(password)) {
+           msg.innerHTML = "사용 가능한 비밀번호 입니다.";
+           msg.style.color = "green";
+       } else {
+           msg.innerHTML = "패스워드 정책에 맞지 않습니다.";
+           msg.style.color = "red";
+       }
+
+
+       const passwordChk = document.getElementById("passwordCheck").value;
+       const msg2 = document.getElementById("PwdcheckMsg");
+       if(password === passwordChk) {
+           msg2.innerHTML = "패스워드가 동일합니다.";
+           msg2.style.color = "green";
+       } else {
+           msg2.innerHTML = "패스워드가 동일하지 않습니다.";
+           msg2.style.color = "red";
+       }
+   }
+
+	// 이메일 유효성 검사
+	 function validateEmail() {
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      const email = document.getElementById("email").value;
+      const msg = document.getElementById("emailMsg");
+
+      if(email == "") {
+           msg.innerHTML = "이메일을 입력하세요.";
+           msg.style.color = "red";
+       } else if(emailRegex.test(email)) {
+           msg.innerHTML = "유효한 이메일 주소입니다.";
+           msg.style.color = "green";
+       } else {
+           msg.innerHTML = "유효하지 않은 이메일 주소입니다.";
+           msg.style.color = "red";
+       }
+    }
