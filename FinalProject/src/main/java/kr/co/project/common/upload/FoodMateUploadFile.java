ebd2ef32d19f3fade4830d28a5ef1ed2ca8dcad1
leesangin <sangin525@naver.com>
2024-02-22 10:49:44 +0900

    레시피 등록중

--- FinalProject/src/main/java/kr/co/project/common/upload/FoodMateUploadFile.java
+++ FinalProject/src/main/java/kr/co/project/common/upload/FoodMateUploadFile.java
@@ -24,7 +24,7 @@ public class FoodMateUploadFile {
 
 
 
-private static final String UPLOAD_PATH="C:\\Users\\suinr\\git\\finalProject\\FinalProject\\src\\main\\webapp\\resources\\uploads\\";
+private static final String UPLOAD_PATH="C:\\Users\\kaw19\\git\\finalProject\\FinalProject\\src\\main\\webapp\\resources\\uploads\\";
 
 
 
